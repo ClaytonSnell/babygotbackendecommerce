92bc9fdce39ac43f652211dfea8b0829ecdfc75d
claytontsnell@gmail.com <claytontsnell@gmial.com>
2022-10-07 09:42:04 -0600

    killed this assignment with tutor

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => console.log(err));
 });
 
 router.get('/:id', (req, res) => {
