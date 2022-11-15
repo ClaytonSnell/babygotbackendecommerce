d69cc45f7bdc39d27e4273bf595fd2ac4b66ea1d
claytontsnell@gmail.com <claytontsnell@gmial.com>
2022-11-15 09:17:22 -0500

    in process of adding routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+  .then((categories) => res.json(categories))
+  .catch((err) => console.log(err));
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    // be sure to include its associated Products
+    indclude: [Product],
+  })
+  .then((category) => res.json(category))
+  .catch((err) => console.log(err))
+  
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
